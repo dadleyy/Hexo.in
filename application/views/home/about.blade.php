49d6dc78a986240d5dec314d0e84c1998a73af42
Danny <dadley@sizethreestudios.com>
2013-04-11 17:06:03 -0400

    Minor update - changed about text

--- application/views/home/about.blade.php
+++ application/views/home/about.blade.php
@@ -12,9 +12,9 @@
         <article class="two">
             <div class="two f cf left">
             <div class="inner">
-                <p class="big">Hexo.in is a multiplayer online game with similarities to <i>roulette</i>.</p> 
-                <p class="reg">Duke it out against your opponent for strategic hexagonal probability and watch as your wins (or losses) rank up. Follow the leader boards to see who is on top or chat with fellow oddballs in the chatrooms after the game.</p>
-                <p class="reg">The game itself was inspired by a mixture of several games, including <i>roulette</i>, <i>hexagon</i> and <i>settlers of catan</i>. The game itself is built using the up-and-coming php framework, <i>laravel</i>. It was built by Danny Hadley as a project for Web Client-Server programming, a course offered by RIT. Currently, there are <?php echo User::count(); ?> players registered.</p>
+                <p class="big">Hexo.in is an online game similar to the popular dart game, <i>501</i>.</p> 
+                <p class="reg">Duke it out against your opponent for strategic hexagonal probability and watch as your wins (or losses) rank up. Battle to keep your territory or fall victim to your opponent's takeover. Follow the leader boards to see who is on top or chat with fellow hexos in the chatrooms after the game.</p>
+                <p class="reg">The game itself was inspired by a mixture of several games, including <i>501</i>, <i>hexagon</i> and <i>the game of life</i>. Hexo.in is powered by the up-and-coming php framework, <i>laravel</i> and was built by Danny Hadley as a senior capstone project. Be sure to check out the blog to follow the creation and development! Currently, there are <?php echo User::count(); ?> players registered. Enjoy!</p>
                 <p class="big">Follow <em class="rale">Hexo.in</em></p>
                 <div class="socials cf">
                     <a href="https://twitter.com/hexoin" title="" class="f icon twitter">
