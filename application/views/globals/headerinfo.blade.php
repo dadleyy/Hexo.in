166f881244bfba3a58415cc5062dc64cda1ff50a
Danny <dadley@sizethreestudios.com>
2013-04-12 00:22:22 -0400

    Minor update - <head> image props

--- application/views/globals/headerinfo.blade.php
+++ application/views/globals/headerinfo.blade.php
@@ -2,9 +2,10 @@
 <title>Hexo.in | {{ $title }}</title>
 <meta name="author" content="Danny Hadley">
 <meta name="application-name" content="Hexo">
-
+<meta name="og:image" content="/img/logo_large.png">
 <link rel="shortcut icon" href="/img/icon.ico" />
 <link rel="icon" type="image/png" href="/img/icon.png" />
+<link rel="image_src" type="image/png" href="/img/logo_large.png" />
 
 <meta name="keywords" content="online, game, multiplayer, laravel, ajax, html5, d3, hexo, danny, hadley, free, javascript, turn-based, fun, 501">
 <meta name="description" content="<?php echo isset( $description ) ? $description : "Duke it out against your opponent for strategic hexagonal domination and watch as your wins (or losses) rank up. Follow the leader boards to see who is on top or chat with fellow oddballs in the chatrooms after the game."; ?>">
