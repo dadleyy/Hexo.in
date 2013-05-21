f8f4ffe8c78659544ec83cb1f9ed5af14e32e290
Danny <dadley@sizethreestudios.com>
2013-05-21 00:38:25 -0400

    Added sitemap bundle to help adsense and analytics

--- application/views/globals/scripts.blade.php
+++ application/views/globals/scripts.blade.php
@@ -27,6 +27,15 @@ _gaq.push(['_trackPageview']);
     
 })( );
 </script>
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-9507342242236137";
+/* Basic */
+google_ad_slot = "6555271408";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
 
 @if( Auth::user() !== null )
 
